823fd77d9548568202d1bfda8fe518da8c8bed3f
yhghfxgh <yhghfxgh@outlook.com>
2026-07-24 11:26:31 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+project = 'google-password-manager-account-login'
+author = 'google-password-manager-account-login'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
